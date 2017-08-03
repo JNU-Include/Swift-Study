90fa1174d508790eaddd14c9d0e240f3ff3f67b7
Lming1 <spear131121@gmail.com>
2017-08-04 01:20:20 +0900

    switch 2

--- Learning Swift.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
+++ Learning Swift.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -81,24 +81,31 @@ if (isRaining || isSnowing) && temperature < 2 {
 
 // Switch
 
-let airportCodes = ["INC", "GMP", "HKG", "SFO", "LHR"]
+let airportCodes = ["LGA", "LHR", "CDG", "HKG", "DXB", "JFK", "LGW", "ORY"]
 
 let airport = airportCodes[0]
 
 for airportCode in airportCodes {
     switch airportCode {
-    case "INC": print("Incheon Airport")
-    case "GMP": print("Gimpo Airport")
-    case "HKG": print("Hong Kong International")
-    case "SFO": print("San Francisco International")
-    case "LHR": print("Heathrow")
+    case "LGA", "JSK": print("New York")
+    case "LHR", "LGW": print("London")
+    case "CDG", "ORY": print("Paris")
+    case "HKG": print("Hong Kong")
     default: print("알 수 없는 공항 코드 입니다.")
     }
 }
 
+import Foundation
+//랜덤으로 숫자 추출
+var randomTemperature = Int(arc4random_uniform(UInt32(80))) - 30
 
-
-
+switch randomTemperature {
+case -20..<0: print("엄청 춥습니다. 외출 자제하세요")
+case 0..<10: print("손이 시려운 날씨입니다. 가급적 장갑을 껴주세요.")
+case 10..<20: print("쌀쌀합니다. 스웨터를 입으세요.")
+case 20..<40: print("덥네요. 티셔츠를 입으세요.")
+default: print("이불 밖은 위험해요.")
+}
 
 
 
