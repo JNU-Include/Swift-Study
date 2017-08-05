d3575ab3164b49937153fb5f14fb6f7b8b35fd20
Lming1 <spear131121@gmail.com>
2017-08-06 00:16:26 +0900

    func 4

--- Learning Swift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Learning Swift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -12,9 +12,16 @@ calculateArea(width: 20, height: 1)
 //Hint : Double
 // C = (F-32) * 5 / 9
 
-func convertToCelsius(temperature : Double) -> Double {
-    let temperatureInCelsius = (temperature-32) * (5/9)
+func convertToCelsius(temperatureInFahrenheit temp : Double) -> Double {
+    let temperatureInCelsius = (temp-32) * (5/9)
     return temperatureInCelsius
 }
 
-convertToCelsius(temperature: 80)
+convertToCelsius(temperatureInFahrenheit: 80)
+
+
+func sayHello(to person: String, and anotherPerson: String) -> String {
+    return "Hello \(person) and \(anotherPerson)"
+}
+
+sayHello(to: "Jack", and: "Jenny")
