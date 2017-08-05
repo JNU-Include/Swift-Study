f70ff62d3fff4cf5f2d0e1a706c6423bd9619af6
Lming1 <spear131121@gmail.com>
2017-08-05 22:51:15 +0900

    func 2

--- Learning Swift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Learning Swift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -1,18 +1,10 @@
-//Wall #1
-let width = 25
-let height = 7
-let area = width * height
-
-//Wall #2
-let secondWidth = 14
-let secondHeight = 3
-let secondArea = secondWidth * secondHeight
-
-func calculateArea() {
-    let width = 25
-    let height = 7
+func calculateArea(width: Int, height: Int) {
     let area = width * height
     print(area)
 }
 
-calculateArea()
+// Wall # 1
+calculateArea(width: 10, height: 20)
+
+// Wall # 2
+calculateArea(width: 20, height: 1)
