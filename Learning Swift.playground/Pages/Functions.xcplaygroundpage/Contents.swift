c7726494d07625a72a7e45a1af3546884bef2667
Lming1 <spear131121@gmail.com>
2017-08-05 23:01:30 +0900

    func 3

--- Learning Swift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Learning Swift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -1,10 +1,20 @@
-func calculateArea(width: Int, height: Int) {
+func calculateArea(width: Int, height: Int) -> Int {
     let area = width * height
-    print(area)
+    return area
 }
 
 // Wall # 1
-calculateArea(width: 10, height: 20)
+let areaOfWall1 = calculateArea(width: 10, height: 20)
 
 // Wall # 2
 calculateArea(width: 20, height: 1)
+
+//Hint : Double
+// C = (F-32) * 5 / 9
+
+func convertToCelsius(temperature : Double) -> Double {
+    let temperatureInCelsius = (temperature-32) * (5/9)
+    return temperatureInCelsius
+}
+
+convertToCelsius(temperature: 80)
