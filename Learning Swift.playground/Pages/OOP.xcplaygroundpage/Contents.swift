f83e737c6c703f7699c566c15e9ec113ffd55941
Lming1 <spear131121@gmail.com>
2017-08-06 20:39:34 +0900

    OOP4

--- Learning Swift.playground/Pages/OOP.xcplaygroundpage/Contents.swift
+++ Learning Swift.playground/Pages/OOP.xcplaygroundpage/Contents.swift
@@ -86,7 +86,89 @@ var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10, height: 1
 let initialCenter = square.center
 square.center = Point(x: 15.0, y: 15.0)
 square.origin
+
 //Type Property - 인스턴스에 연관되지 않음.
+
+class SomeClass {
+    static var storedTypeProperty = "Some value"
+    static var computedTypeProperty: Int {
+        return 27
+    }
+}
+
+SomeClass.storedTypeProperty
+SomeClass.computedTypeProperty
+
+//Property Observer - willSet didSet
+
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set total steps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+stepCounter.totalSteps = 360
+stepCounter.totalSteps = 940
+
+//Methods
+class Counter {
+    var count = 0
+    
+    func increment() {
+        count += 1
+    }
+    
+    func increment(by amount: Int){
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+}
+
+
+let counter = Counter()
+counter.increment()
+counter.count
+counter.increment(by: 20)
+counter.count
+counter.reset()
+counter.count
+
+
+//Self property
+struct Point2 {
+    var x = 0.0
+    var y = 0.0
+    
+    func isTotheRightOf(x: Double) -> Bool {
+        return self.x > x
+    }
+}
+
+
+//Type method
+
+class SomeClass2 {
+    static func someTypeMethod() {
+        print("some type method")
+    }
+}
+SomeClass2.someTypeMethod()
+
+
+
+
+
 
 
 
