504aa7539a768bb9d2c87ee84ddb410243c0deaa
Lming1 <spear131121@gmail.com>
2017-08-09 19:13:59 +0900

    Image Picker Add

--- Include AI/Include AI/View Controller/ObjectDetectionViewController.swift
+++ Include AI/Include AI/View Controller/ObjectDetectionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ObjectDetectionViewController: UIViewController {
+class ObjectDetectionViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +21,20 @@ class ObjectDetectionViewController: UIViewController {
         let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
         let importFromAlbum = UIAlertAction(title: "앨범에서 선택하기", style: .default) { _ in
-            
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .savedPhotosAlbum
+            picker.allowsEditing = true
+            self.present(picker, animated: true, completion: nil)
         }
         
         let takePhoto = UIAlertAction(title: "카메라로 찍기", style: .default) { _ in
-            
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .camera
+            picker.cameraCaptureMode = .photo
+            picker.allowsEditing = true
+            self.present(picker, animated: true, completion: nil)
         }
         
         let cancle = UIAlertAction(title: "취소하기", style: .cancel)
@@ -35,6 +44,16 @@ class ObjectDetectionViewController: UIViewController {
         actionSheet.addAction(cancle)
         
         self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true)
+        if let uiImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            print("이미지를 성공적으로 가져왔습니다.")
+            
+            
+            
+        }
     }
     
 
